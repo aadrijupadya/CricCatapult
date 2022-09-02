631da8a9f51b7d12a44c54a576976ed5b9284888
Aadrij Upadya <adrij2005@gmail.com>
2022-09-02 00:01:45 -0700

    Setup.py

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Package for cricket analytics'
+LONG_DESCRIPTION = 'Visualizaiton, Analysis, and more (will add later)'
+
+# Setting up
+setup(
+    name="criccatapult",
+    version=VERSION,
+    author="Aadrij Upadya, Pradyum Chitlu",
+    author_email="<adrij2005@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'matplotlib',
+                      'requests', 'bs4', 'geocoder', 'folium', 'geopandas', 'seaborn', 'pygame', 'ssl', 'zipfile', 'io'],
+    keywords=['python', 'cricket', 'sports analytics',
+              'data analytics', 'visualiation', 'geospatial data', 'GIS'],
+    classifiers=[
+        "Development Status :: 2 - Production",
+        "Intended Audience :: Sports Analysts/General Fans",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
