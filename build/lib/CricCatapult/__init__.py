8c70b151e4bec38b2dad2bb840b6a2615b2623c6
Aadrij Upadya <adrij2005@gmail.com>
2022-09-10 12:04:55 -0700

    Fixing issues with Cricsheet

--- build/lib/CricCatapult/__init__.py
+++ build/lib/CricCatapult/__init__.py
@@ -1,6 +1,6 @@
 from CricCatapult.Series import Series
 from CricCatapult.Records import Records
 from CricCatapult.Location import Location
-from CricCatapult.cricsheet import cricsheet
+from CricCatapult.Cricsheet import Cricsheet
 from CricCatapult.Player import Player
 from CricCatapult.Scoreboard import Scoreboard
